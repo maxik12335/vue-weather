7b3182216d716e6568c3336057c0b6d78cc7dd48
Maxim Kuznetsov <m.kuznecov@krediska.ru>
2022-11-08 14:21:13 +0300

    Улучшил код и сделал ui

--- src/components/CardCity.vue
+++ src/components/CardCity.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="card-city">
-      <h2 class="card-city__title">{{ title }}</h2>
-      <p class="card-city__temperature">{{ temperature }}</p>
+      <h2 class="card-city__name">{{ cityData.name }}</h2>
+      <p class="card-city__temperature">Темпартура: {{ cityData.temperature }} 	&#176;С</p>
     </div>
   </div>  
 </template>
@@ -10,22 +10,32 @@
 <script>
 export default {
   props: {
-    title: "",
-    temperature: "",
+    cityData: {
+      type: Object,
+      required: true,
+    }
   }
 }
 </script>
 
 <style>
 .card-city {
-
+  width: 500px;
+  height: 300px;
+  border: 1px solid;
+  margin: 50px auto;
+  padding: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
 }
 
-.card-city__title {
+.card-city__name{
 
 }
 
 .card-city__temperature {
-
+  font-size: 25px;
 }
 </style>
